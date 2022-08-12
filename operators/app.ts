4dd2732f5a6f336585eeb8697833a578bbe63d99
Muhammad Asmar Naeem <110281598+MAsmarNaeem@users.noreply.github.com>
2022-08-12 16:35:44 -0700

    operators

---
+++ operators/app.ts
@@ -0,0 +1,78 @@
+
+
+// // var number: any = 10
+// // var number1: any = 20
+// // var number2: boolean = number < number1
+
+// // console.log("number1 is greater than number", number2)
+
+// // //loical operator
+
+// // var  num1:any=49
+// // var num2:any=30
+// // console.log("number 1:", num1,"number 2:",num2)
+
+// // var num4:boolean=((num1>num2)&&(num1<num2))
+// // var num3:boolean=!((num1>num2)||(num1<num2))
+
+
+// // console.log("result4 is ",num4)
+// // console.log("result3 is ",num3)
+
+// var a :any=30;
+// var b:any=20;
+
+
+//  a-=b
+   
+   
+// console.log("result is a",a)
+
+// //conditioal operator
+
+// var a:any=10
+// var b:any=20
+
+// result=a>b? "true" : "false"
+
+// console.log(result)
+
+// var num:any=-3;
+// result1=-num<0 ? "positive" : "non positive";
+
+// console.log(result1)
+
+// var a=true
+// console.log(typeof a)
+// a=10
+// console.log(typeof a)
+// a="10"
+// console.log(typeof a)
+// a=11.4
+// console.log(typeof a)
+
+
+// var a=10
+// var b=20
+// var c=a+b
+// console.log(c)
+
+// a="asmar"
+// b=" naeem"
+// c=a+b
+// console.log(c)
+
+//  var flag:boolean=true
+//  console.log(typeof flag)
+
+//   flag:number=10
+//  console.log(typeof flag)
+
+
+
+var num:number= 12;
+console.log(typeof num);
+num="hlo";
+
+console.log(typeof num);
+
